baa6347f6e7e97f18224bdb8154305256e9876a4
Nuno Marques <nunomarques@makeadream.pt>
2024-02-15 09:30:49 +0000

    fix nuxt base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/btl/", // baseURL: '/<repository>/'
+  },
   components: true,
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-swiper", "@nuxt/content"],
