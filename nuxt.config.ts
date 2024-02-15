8c9c9c64bea9fef5f73e4c9875a3242f3606aae4
Nuno Marques <nunomarques@makeadream.pt>
2024-02-15 09:37:21 +0000

    add buildAssetsDir to nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   app: {
     baseURL: "/btl/", // baseURL: '/<repository>/'
+    buildAssetsDir: "/btl/",
   },
   components: true,
   devtools: { enabled: true },
