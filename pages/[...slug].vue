48c136969c52533349f9dc57ae8d37b3cdfdf0fa
Nuno Marques <nunomarques@makeadream.pt>
2024-03-20 00:27:11 +0000

    feat: mobile add btn back

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -212,6 +212,24 @@
             class="mt-16 text-sm font-semibold prose 3xl:prose-2xl"
             v-html="hotelData.contacts"
           ></div>
+          <div class="flex justify-end mt-10">
+          <NuxtLink to="/" class="btn btn_back"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-6 h-6 md:w-8 md:h-8 transform rotate-180 3xl:w-14 3xl:h-14"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 6l6 6l-6 6" />
+            </svg>
+            Back</NuxtLink
+          >
+          </div>
       </div>
 
     </div>
